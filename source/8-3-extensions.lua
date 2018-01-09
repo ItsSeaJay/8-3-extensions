ab0e20c0f43400f45340dd4214bda5fb6492d75d
Unknown <b6010572@my.shu.ac.uk>
2018-01-09 21:56:03 +0000

    Created letters table

---
+++ source/8-3-extensions.lua
@@ -0,0 +1,13 @@
+--[[
+  8:3 Extensions
+  by
+  Callum John (@ItsSeaJay)
+]]
+
+path = "extensions.txt"
+file = io.open(path)
+letters = {
+  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",
+  "n",  "o",  "p",  "q",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",
+  "z"
+}
