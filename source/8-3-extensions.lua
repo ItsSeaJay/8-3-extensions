161cecc072aae8dfa3a36c8bed23662fba58f171
Unknown <b6010572@my.shu.ac.uk>
2018-01-09 22:10:52 +0000

    Fixed iteration error

--- source/8-3-extensions.lua
+++ source/8-3-extensions.lua
@@ -5,9 +5,18 @@
 ]]
 
 path = "extensions.txt"
-file = io.open(path)
+file = io.open(path, "w")
 letters = {
   "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",
   "n",  "o",  "p",  "q",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",
   "z"
 }
+
+io.output(file)
+
+for i in pairs(letters) do
+  io.write("a")
+  print("a")
+end
+
+io.close(file)
